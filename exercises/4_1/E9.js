08f2ad4358fe4911aec7dfc11dff365c3f9b7e6e
Marco Barbosa <marco.meireles.b@gmail.com>
2020-02-14 18:20:53 -0300

    [R] Exercicio 9

---
+++ exercises/4_1/E9.js
@@ -0,0 +1,10 @@
+//Programa para saber se existe numero ímpar em três numeros
+const a=34;
+const b=26;
+const c= 18;
+if(a%2==1||b%2==1||c%2==1){
+    console.log(true);
+}
+else {
+    console.log(false);
+}
