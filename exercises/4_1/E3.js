9a3d2bf4247886f3783e8b8033562cf1a596f985
Marco Barbosa <marco.meireles.b@gmail.com>
2020-02-14 17:06:06 -0300

    [R] Exercicio 3

---
+++ exercises/4_1/E3.js
@@ -0,0 +1,19 @@
+const a =15;
+const b =42;
+const c =19;
+if(a>b){
+   if(a>c){
+      console.log(a);
+   }
+   else {
+       console.log(c);
+   }
+}
+else{
+    if(b>c){
+        console.log(b);
+    }
+    else{
+        console.log(c);
+    }
+}
