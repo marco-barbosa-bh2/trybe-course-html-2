b66a57f0b999bde31b9c66ce578fbff1eb383b70
Marco Barbosa <marco.meireles.b@gmail.com>
2020-02-17 20:30:42 -0300

    [P]Exercicio 4

--- exercises/4_2/E4.js
+++ exercises/4_2/E4.js
@@ -1,8 +1,21 @@
+//Programa para gerar uma piramide de base n
 const n=5;
 
 let tela="01234";
 
 for(let i=0;i<n;i+=1){
-    tela=tela.replace(tela[i],"*");
-    console.log(tela);
+    
+    
+    if(i==2){
+        tela=tela.replace(tela[parseInt(n/2)],"*");     
+    }
+
+    //if(i!=parseInt(n/2)){
+    //    tela=tela.replace(tela[i]," ");
+    //}   
+   
+    if(i==(n-1)){
+       console.log(tela);
+    }
 }
+
