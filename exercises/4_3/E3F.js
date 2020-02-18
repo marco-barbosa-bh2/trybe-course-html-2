23d7bfe7d4883e6e359647b460563f5765cb59bb
Marco Barbosa <marco.meireles.b@gmail.com>
2020-02-18 18:35:38 -0300

    [R] Exercicio 4 Funcoes

--- exercises/4_3/E3F.js
+++ exercises/4_3/E3F.js
@@ -1,6 +1,6 @@
 const inteiros=[2,3,6,7,10,0,-3];
 
-function verificaMaior(lista){
+function verificaMenor(lista){
     let menor=lista;
     let ultimo=0;
     let i=0;
@@ -16,4 +16,4 @@ function verificaMaior(lista){
     return console.log(i);
 }
 
-verificaMaior(inteiros);
+verificaMenor(inteiros);
