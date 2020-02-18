638c64976f57a71a4b93ce9544babeff83b85394
Marco Barbosa <marco.meireles.b@gmail.com>
2020-02-18 17:21:46 -0300

    [R]Exercico 2 Objeto / For/In

---
+++ exercises/4_3/E2.js
@@ -0,0 +1,9 @@
+//Imprime objeto
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    recorrente: "Sim",
+  };
+
+console.log(info);
