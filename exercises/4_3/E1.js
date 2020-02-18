189ceb63924c4610c87fb9ddf3b240724e704c0f
Marco Barbosa <marco.meireles.b@gmail.com>
2020-02-18 17:17:52 -0300

    [R] Exercicio 1 - Objeto / For/In

---
+++ exercises/4_3/E1.js
@@ -0,0 +1,8 @@
+//Mensagem de saudação
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  };
+
+console.log("Bem vinda, ",info.personagem);
