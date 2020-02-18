5bd43e18bf0d51294c167ba079ac7e966f7e1c1a
Marco Barbosa <marco.meireles.b@gmail.com>
2020-02-18 17:31:44 -0300

    [R] Exercicio 4 Objeto / For/In

---
+++ exercises/4_3/E4.js
@@ -0,0 +1,11 @@
+//Imprime valores do objeto com for in
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    recorrente: "Sim",
+  };
+
+for(indice in info){
+    console.log(info[indice]);
+}
