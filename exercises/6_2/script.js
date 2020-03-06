a7f72264bfd45b0960f07a63a21761e97333b0ac
Marco Barbosa <marco.meireles.b@gmail.com>
2020-03-06 20:32:31 -0300

    Exercicio 3

--- exercises/6_2/script.js
+++ exercises/6_2/script.js
@@ -11,34 +11,9 @@ estado.addEventListener('mouseover', function () {
   }  
 });
 
-dataInicio.DatePickerX.init();
-
-/*
-dataInicio.addEventListener('keyup', function () {
-  const dataInserida = dataInicio.value;
-  let dataSeparada = [];
-  
-  if (dataInserida.length === 10) {
-    dataSeparada = dataInserida.split('/');
-  }
-
-  if(parseInt(dataSeparada[0]) < 0 || parseInt(dataSeparada[0]) > 31){
-    data.innerHTML = '- Dia do Mês Inválido';
-  } else {
-    data.innerHTML = '';
-  }
-
-  if(parseInt(dataSeparada[1]) < 0 || parseInt(dataSeparada[1]) > 12){
-    data.innerHTML += ' - Mês Inválido';
-  } else {
-    data.innerHTML += '';
-  }
-
-  if(parseInt(dataSeparada[2]) < 0){
-    data.innerHTML += ' - Ano Inválido';
-  } else {
-    data.innerHTML += '';
-  }
-
+dataInicio.DatePickerX.init({
+  format : 'dd/mm/yyyy',
+  clearButtonLabel : 'Limpa',
+  todayButtonLabel : 'Hoje',
 });
-*/
+
