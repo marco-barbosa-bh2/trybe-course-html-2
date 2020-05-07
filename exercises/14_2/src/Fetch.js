a7ff2f1404da33b7d2fb8ff657df923dfd498506
Marco Barbosa <marco.meireles.b@gmail.com>
2020-05-07 18:59:49 -0300

    Exercicio Resolvido

--- exercises/14_2/src/Fetch.js
+++ exercises/14_2/src/Fetch.js
@@ -15,18 +15,32 @@ class Fetch extends React.Component {
   componentDidMount() {
     fetch(this.props.url)
     .then(data => data.json())
-    .then(dataJson => this.setState(()=>({src:[dataJson.message]})));
+    .then(dataJson => this.setState(()=>({src:dataJson.message})));
     if(this.state.src) this.dogName();
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('Próximo Update: ', nextState.src);
+    if (this.dogName(nextState.src).includes('terrier')) {
+      this.componentWillUnmount();
+      return false;
+    }
+    return true; 
+  }
+  
   componentDidUpdate() {
-    if(this.state.src) this.dogName();
+    if(this.state.src) alert(this.dogName());
+  }
+
+  componentWillUnmount() {
+    console.log('Desmontei');
+    this.setState(() => ({src:'https://images.dog.ceo/breeds/springer-english/n02102040_7555.jpg'}));
   }
 
   nextDogReady() {
     fetch(this.props.url)
     .then(data => data.json())
-    .then(dataJson => this.setState(()=>({src:[dataJson.message]})));
+    .then(dataJson => this.setState(()=>({src:dataJson.message})));
   }
 
   nextDog() {
@@ -35,15 +49,17 @@ class Fetch extends React.Component {
     );
   }
 
-  dogName() {
-    const urlString = this.state.src.toString();
+  dogName(url) {
+    let urlString = '';
+    if (url) { urlString = url.toString(); }
+    else { urlString = this.state.src.toString(); }
     let newUrlString = '';
     let raca = '';
     const start = urlString.indexOf('breeds/') + 7;
     newUrlString = urlString.slice(start,urlString.length); 
     const end = newUrlString.indexOf('/');
     raca = newUrlString.slice(0,end);
-    alert(raca);
+    return raca;
   }
 
 
