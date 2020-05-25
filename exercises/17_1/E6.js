c0eca0ccdae7bb5d3f98b5784fc5b82a0afaf501
Marco Barbosa <marco.meireles.b@gmail.com>
2020-05-25 18:51:54 -0300

    Exercicio 6 Resolvido

---
+++ exercises/17_1/E6.js
@@ -0,0 +1,28 @@
+// Redux exercises
+const ADD = 'ADD';
+
+const reducer = (state = 0, action) => {
+  switch(action.type) {
+    case ADD:
+      return state + 1;
+    default:
+      return state;
+  }
+};
+
+const store = Redux.createStore(reducer);
+
+// global count variable:
+let count = 0;
+
+// change code below this line
+const lookCount = () =>   count += 1
+store.subscribe(lookCount);
+// change code above this line
+
+store.dispatch({type: ADD});
+console.log(count);
+store.dispatch({type: ADD});
+console.log(count);
+store.dispatch({type: ADD});
+console.log(count);
