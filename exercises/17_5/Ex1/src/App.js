96bbaa3817cc2b55c5466d97cc8d5f1abfa1ff9b
Marco Barbosa <marco.meireles.b@gmail.com>
2020-05-29 23:16:15 -0300

    Exercicios 1 e 2 resolvidos

---
+++ exercises/17_5/Ex1/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Counter from './components/Counter';
+import Button from './components/Button';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Counter />
+      <Button />
+    </div>
+  );
+}
+
+export default App;
