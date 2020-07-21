a2dabbadc1f3b87ad31409f565f58d137f9415c9
Marco Barbosa <marco.meireles.b@gmail.com>
2020-07-21 19:57:04 -0300

    Exercicio 1 do 1.15 ao 1.16

--- exercises/exercises/21_3/E1.sql
+++ exercises/exercises/21_3/E1.sql
@@ -66,3 +66,35 @@ GROUP BY Manufacturers.Name
 HAVING AVG(Price) >= 150;
 
 -- 1.15
+SELECT name, price FROM betrybe.Products
+ORDER BY price ASC
+LIMIT 1;
+
+-- 1.16
+SELECT m.Name, MAX(p.Price) AS price
+FROM betrybe.Products AS p, betrybe.Manufacturers AS m
+WHERE p.Manufacturer = m.Code
+GROUP BY m.Name;
+
+SELECT p.*, m.name AS manu_name
+FROM betrybe.Products AS p
+INNER JOIN betrybe.Manufacturers AS m
+ON (p.manufacturer = m.code);
+
+SELECT max_price_mapping.name AS manu_name,
+max_price_mapping.price,
+products_with_manu_name.name AS product_name
+FROM
+(SELECT m.Name, MAX(p.Price) AS price
+FROM betrybe.Products AS p, betrybe.Manufacturers AS m
+WHERE p.Manufacturer = m.Code
+GROUP BY m.Name) AS max_price_mapping
+LEFT JOIN
+(SELECT q.*, n.name AS manu_name
+FROM betrybe.Products AS q
+INNER JOIN betrybe.Manufacturers AS n
+ON (q.manufacturer = n.code)) AS products_with_manu_name
+ON
+(max_price_mapping.name = products_with_manu_name.manu_name
+and
+max_price_mapping.price = products_with_manu_name.price);
