af7888b21079c407ef16b6a73a487bdfbc37c741
Marco Barbosa <marco.meireles.b@gmail.com>
2020-07-23 18:40:53 -0300

    Exercicio 3 (Renomeado exercicio 1)

---
+++ exercises/exercises/22_1/E2.sql
@@ -0,0 +1,13 @@
+Ref: https://www.sanfoundry.com/sql-mcqs-mysql-datatypes-1/
+
+1) c
+2) a
+3) a
+4) a
+5) a
+6) c
+7) a
+8) a
+9) d
+10) c
+
---
+++ exercises/exercises/22_1/E3.sql
@@ -0,0 +1,12 @@
+Ref: https://www.sanfoundry.com/sql-mcqs-mysql-datatypes-2/
+
+1) c
+2) c
+3) b
+4) d
+5) c
+6) a
+7) d
+8) c
+9) a
+
