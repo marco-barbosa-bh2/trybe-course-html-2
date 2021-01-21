438e75b30dcad7b953a69a43d1d07363f5210b9c
Marco Barbosa <marco.meireles.b@gmail.com>
2021-01-20 23:01:16 -0300

    exercicio 1 resolvido

--- exercises/estrutura-de-dados-pilhas-filas-e-listas_4/ex1.py
+++ exercises/estrutura-de-dados-pilhas-filas-e-listas_4/ex1.py
@@ -3,25 +3,84 @@ from doubly_linked_list import DoublyLinkedList
 
 def remove_copys(linked_list):
     a = list()
-    have_been_listed = list()
+    b = list()
+    c = list()
+
+    # coloca os valores dos nós em uma lista
     for n in range(len(linked_list)):
         node = linked_list.remove_first()
         if node is not None:
-            if node.value not in have_been_listed:
-                a.append(node.value)
-            else:
-                have_been_listed.append(node.value)
-    print(a)
+            a.append((node.value, n))
+
+    # ordena a lista
+    a.sort()
+
+    # acrescenta um último elemento na lista
+    # fora de ordem que vai nos ajudar na hora
+    # de iterar sobre a lista
+    a.append((0, -1))
+
+    # cria uma segunda lista apenas com os elementos
+    # que nao se repetem
+    for m in range(0, len(a) - 1):
+        if a[m + 1][0] == a[m][0] or a[m][0] == a[m - 1][0]:
+            None
+        else:
+            b.append(a[m])
+
+    # ref.: http://bit.ly/2LOiMME
+    # ordena uma terceira lista pelo número do nó original
+    c = sorted(b, key=lambda x: x[1])
+
+    # cria uma nova lista
+    new_linked_list = DoublyLinkedList()
+
+    # insere os elementos na nova lista
+    for element in c:
+        new_linked_list.insert_last(element[0])
+
+    return new_linked_list
 
 
 if __name__ == "__main__":
     linked_list = DoublyLinkedList()
-    linked_list.insert_last(5)
-    linked_list.insert_last(5)
-    linked_list.insert_last(4)
-    linked_list.insert_last(4)
-    linked_list.insert_last(3)
-    linked_list.insert_last(2)
-    linked_list.insert_last(1)
+    print("________________________________________")
 
-    remove_copys(linked_list)
+    # teste 1
+    linked_list.insert_last(1)
+    linked_list.insert_last(1)
+    linked_list.insert_last(2)
+
+    print(f"lista original: {linked_list}")
+    print(f"nova lista: {remove_copys(linked_list)}")
+    print("________________________________________")
+
+    # limpa a lista
+    linked_list = DoublyLinkedList()
+
+    # teste 2
+    linked_list.insert_last(1)
+    linked_list.insert_last(1)
+    linked_list.insert_last(2)
+    linked_list.insert_last(3)
+    linked_list.insert_last(3)
+
+    print(f"lista original: {linked_list}")
+    print(f"nova lista: {remove_copys(linked_list)}")
+    print("________________________________________")
+
+    # limpa a lista
+    linked_list = DoublyLinkedList()
+
+    # teste 3
+    linked_list.insert_last(1)
+    linked_list.insert_last(2)
+    linked_list.insert_last(3)
+    linked_list.insert_last(3)
+
+    print(f"lista original: {linked_list}")
+    print(f"nova lista: {remove_copys(linked_list)}")
+    print("________________________________________")
+
+    # limpa a lista
+    linked_list = DoublyLinkedList()
